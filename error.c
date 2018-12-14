58c892b81fc0496475d9493fd37d3082be43b887
nxtxnt <laurentengelbrecht1@gmail.com>
2018-12-14 15:26:54 +0100

    Update error.c

--- error.c
+++ error.c
@@ -1,5 +1,5 @@
 #include "main.h"
 
 int error(char *message, errno err_type) {
-  printf("Error : %s, error code 0x%hhX\n", message, err_type);
+  printf("Error : %s, error code 0x%xx\n", message, err_type);
 }
